ba108e149afdf0b92fef78fd4250d0d67346ad5d
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2021-12-30 22:38:13 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "PIG.h"
 #include "personagem.h"
+#include "mundo.h"
+#include <vector>
 
 PIG_Evento evento;          //evento ser tratado a cada passada do loop principal
 PIG_Teclado meuTeclado;     //variável como mapeamento do teclado
 
+void DesenhaCoracoes(vector<int> c) {
+    for(int i = 0; i < c.size(); i++) {
+        DesenhaObjeto(c[i]);
+    }
+}
 
 int main( int argc, char* args[] ){
 
@@ -25,9 +32,18 @@ int main( int argc, char* args[] ){
     player.CriaPersonagem("..//imagens//sprite andando.png", "..//imagens//spritePersonagem.txt");
     player.SetTimerTeclado(CriaTimer());
 
-    int chao = CriaObjeto("..//imagens//chao.png");
-    MoveObjeto(chao,500, 0);
-    SetDimensoesObjeto(chao, 50,100);
+    Mundo mundo(20);
+    mundo.CriaChao();
+
+    vector<int> coracoes;
+
+    for(int i = 0; i < player.getVida(); i++) {
+        int coracao = CriaObjeto("..//imagens//coracao.png");
+        MoveObjeto(coracao, 100 + i*30, PIG_ALT_TELA-100);
+        SetDimensoesObjeto(coracao, 30, 30);
+        coracoes.push_back(coracao);
+    }
+
     int xChao, yChao;
     //loop principal do jogo
     while(JogoRodando()) {
@@ -38,7 +54,7 @@ int main( int argc, char* args[] ){
 
         //aqui o evento deve ser tratado e tudo deve ser atualizado
 
-        player.MovePersonagem(meuTeclado, chao, xChao, yChao);
+        player.MovePersonagem(meuTeclado, mundo.plataformas[1], xChao, yChao);
 
         //será feita a preparação do frame que será exibido na tela
         IniciaDesenho();
@@ -46,11 +62,12 @@ int main( int argc, char* args[] ){
         //todas as chamadas de desenho devem ser feitas aqui na ordem desejada
 
         DesenhaObjeto(player.getObjeto());
-        DesenhaObjeto(chao);
+        mundo.DesenhaChao();
 
         PreparaCameraFixa();
 
-        EscreveDoubleEsquerda(GetFPS(), 1, 0, PIG_ALT_TELA - 100, {255,0,255,255});
+       //DesenhaCoracoes(coracoes);
+        EscreveDoubleDireita(GetFPS(), 1, PIG_LARG_TELA-100, PIG_ALT_TELA - 100, {255,0,255,255});
         EscreveInteiroCentralizado(player.getX(), 100, 300);
         EscreveInteiroCentralizado(player.getY(), 100, 350);
 
