a63126d3cf2a914a9d5fc7c7ee6fcaa15113d0f0
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2021-12-30 22:39:39 -0300

    Add files via upload

---
+++ src/mundo.h
@@ -0,0 +1,32 @@
+#ifndef MUNDO_H_INCLUDED
+#define MUNDO_H_INCLUDED
+
+#include <vector>
+
+class Mundo {
+public:
+    int qtdPisos;
+    vector<int> plataformas;
+
+    Mundo(int pisos) {
+        qtdPisos = pisos;
+    }
+
+    void CriaChao() {
+        for( int i = 0; i < qtdPisos; i++) {
+            int chao = CriaObjeto("..//imagens//chao.png");
+            MoveObjeto(chao,i*100, 0);
+            SetDimensoesObjeto(chao, 50, 100);
+            plataformas.push_back(chao);
+        }
+    }
+
+    void DesenhaChao() {
+        for(int i = 0; i < qtdPisos; i++) {
+            DesenhaObjeto(plataformas[i]);
+        }
+    }
+};
+
+
+#endif // MUNDO_H_INCLUDED
