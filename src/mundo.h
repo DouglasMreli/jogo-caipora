0d4d8dec10a1d05624ff2380b3b7b850aafdb28d
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2022-01-03 20:42:00 -0300

    Update mundo.h

--- src/mundo.h
+++ src/mundo.h
@@ -15,7 +15,13 @@ public:
     void CriaChao() {
         for( int i = 0; i < qtdPisos; i++) {
             int chao = CriaObjeto("..//imagens//chao.png");
-            MoveObjeto(chao,i*100, 0);
+            if(i == 3) {
+                MoveObjeto(chao,i*100, 180);
+            }else if(i == 5) {
+                MoveObjeto(chao,i*100, 50);
+            }else{
+                MoveObjeto(chao,i*100, 0);
+            }
             SetDimensoesObjeto(chao, 50, 100);
             plataformas.push_back(chao);
         }
