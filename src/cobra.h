ae80ee03a07e036ee364ca0ec1a8c210ccfc2510
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2022-01-03 20:55:28 -0300

    Add files via upload

---
+++ src/cobra.h
@@ -0,0 +1,23 @@
+#ifndef COBRA_H_INCLUDED
+#define COBRA_H_INCLUDED
+
+/*
+    Classe responsável por gerenciar os inimigos-cobra das fases
+*/
+
+class Cobra {
+
+private:
+    int vida = 3; // variável que guarda a vida do inimigo-cobra
+    int x, y;     // posição do inimigo-cobra no mundo
+    int animacao; // guarda a animação do inimigo-cobra
+
+
+
+public:
+
+
+
+};
+
+#endif // COBRA_H_INCLUDED
