0c5b905806c3edc4b91583f47b4b93d345b61851
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2022-01-03 20:56:18 -0300

    Update mundo.h

--- src/mundo.h
+++ src/mundo.h
@@ -5,14 +5,17 @@
 
 class Mundo {
 public:
-    int qtdPisos;
-    vector<int> plataformas;
+    int qtdPisos;     //  variavel que guarda a quantiade de pisos da fase
+    vector<int> plataformas;  // vetor que guarda as plataformas da fase
 
+    // construtor da classe que recebe a quantidade de pisos
     Mundo(int pisos) {
         qtdPisos = pisos;
     }
 
+    // Função responsável por criar as plataformas da fase
     void CriaChao() {
+        //carrega a plataforma do arquivo e as posicona no mundo
         for( int i = 0; i < qtdPisos; i++) {
             int chao = CriaObjeto("..//imagens//chao.png");
             if(i == 3) {
@@ -23,10 +26,11 @@ public:
                 MoveObjeto(chao,i*100, 0);
             }
             SetDimensoesObjeto(chao, 50, 100);
-            plataformas.push_back(chao);
+            plataformas.push_back(chao);      // Adiciona a nova plataforma na lista
         }
     }
 
+    // Função responsável por desenhar o chão na tela
     void DesenhaChao() {
         for(int i = 0; i < qtdPisos; i++) {
             DesenhaObjeto(plataformas[i]);
